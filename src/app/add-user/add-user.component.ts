6152d6576f0983aa78aa23fa7192eacc2c3aebbf
saloni <salonip1993@gmail.com>
2018-10-01 15:28:21 +0530

    changes

---
+++ src/app/add-user/add-user.component.ts
@@ -0,0 +1,83 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {UserService} from "../user.service";
+import {first} from "rxjs/operators";
+import {Router} from "@angular/router";
+
+
+@Component({
+  selector: 'app-add-user',
+  templateUrl: './add-user.component.html',
+  styleUrls: ['./add-user.component.css']
+})
+export class AddUserComponent implements OnInit {
+  constructor(private formBuilder: FormBuilder,private router: Router, private userService: UserService) { }
+
+  addForm: FormGroup;
+
+  ngOnInit() {
+
+    this.addForm = this.formBuilder.group({
+      id: [],
+      email: ['', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required]
+    });
+
+  }
+
+  onSubmit() {
+    this.userService.createUser(this.addForm.value)
+      .subscribe( data => {
+        console.log(">>>>data add user>>>>>>",data)
+        this.router.navigate(['list-user']);
+      });
+  }
+
+  /* // At the drag drop area
+  // (drop)="onDropFile($event)"
+  onDropFile(event: DragEvent) {
+    event.preventDefault();
+    this.uploadFile(event.dataTransfer.files);
+  }
+
+  // At the drag drop area
+  // (dragover)="onDragOverFile($event)"
+  onDragOverFile(event) {
+    event.stopPropagation();
+    event.preventDefault();
+  }
+
+  // At the file input element
+  // (change)="selectFile($event)"
+  selectFile(event) {
+    this.uploadFile(event.target.files);
+  }
+
+  uploadFile(files: FileList) {
+    if (files.length == 0) {
+      console.log("No file selected!");
+      return
+
+    }
+    let file: File = files[0];
+
+    this.upload.uploadFile(this.appCfg.baseUrl + "/api/flash/upload", file)
+      .subscribe(
+        event => {
+          if (event.type == HttpEventType.UploadProgress) {
+            const percentDone = Math.round(100 * event.loaded / event.total);
+            console.log(`File is ${percentDone}% loaded.`);
+          } else if (event instanceof HttpResponse) {
+            console.log('File is completely loaded!');
+          }
+        },
+        (err) => {
+          console.log("Upload Error:", err);
+        }, () => {
+          console.log("Upload done");
+        }
+      )
+  }
+ */
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AddUserComponent } from './add-user/add-user.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
+import { ListUserComponent } from './list-user/list-user.component';
+import { routing } from './/app-routing.module';
+
+import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {UserService} from "./user.service";
+import { AuthService } from 'src/app/auth.service';
+import { PagerService } from "./pager.service";
+
+
+import { FormsModule } from '@angular/forms';
+import {FilterPipe} from './filterPipe'
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AddUserComponent,
+    EditUserComponent,
+    ListUserComponent,
+    FilterPipe 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule,
+    
   ],
-  providers: [],
+  providers: [UserService,AuthService,PagerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
